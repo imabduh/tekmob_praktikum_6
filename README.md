416a314799118536527d1bf2196bcde9c948ea75
imabduh <imuhammadabduh@gmail.com>
2024-06-04 22:55:04 +0700

    memperbaiki error theme yang tampil

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Praktikum 5
+# Praktikum 6
 NIM: 2100016008
 Nama: Muhammad Abduh
--- lib/app.dart
+++ lib/app.dart
@@ -52,30 +52,30 @@ ThemeData _buildShrineTheme() {
 }
 
 // TODO: Build a Shrine Text Theme (103)
-TextTheme _buildShrineTextTheme(TextTheme base) {
-  return base
-      .copyWith(
-        headline5: base.headline5!.copyWith(
-          fontWeight: FontWeight.w500,
-        ),
-        headline6: base.headline6!.copyWith(
-          fontSize: 18.0,
-        ),
-        caption: base.caption!.copyWith(
-          fontWeight: FontWeight.w400,
-          fontSize: 14.0,
-        ),
-        bodyText1: base.bodyText1!.copyWith(
-          fontWeight: FontWeight.w500,
-          fontSize: 16.0,
-        ),
-      )
-      .apply(
-        fontFamily: 'Rubik',
-        displayColor: kShrineBrown900,
-        bodyColor: kShrineBrown900,
-      );
-}
+// TextTheme _buildShrineTextTheme(TextTheme base) {
+//   return base
+//       .copyWith(
+//         headline5: base.headline5!.copyWith(
+//           fontWeight: FontWeight.w500,
+//         ),
+//         headline6: base.headline6!.copyWith(
+//           fontSize: 18.0,
+//         ),
+//         caption: base.caption!.copyWith(
+//           fontWeight: FontWeight.w400,
+//           fontSize: 14.0,
+//         ),
+//         bodyText1: base.bodyText1!.copyWith(
+//           fontWeight: FontWeight.w500,
+//           fontSize: 16.0,
+//         ),
+//       )
+//       .apply(
+//         fontFamily: 'Rubik',
+//         displayColor: kShrineBrown900,
+//         bodyColor: kShrineBrown900,
+//       );
+// }
 
 // TODO: Convert ShrineApp to stateful widget (104)
 class ShrineApp extends StatelessWidget {
